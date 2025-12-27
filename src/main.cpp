59d5972a85891cb3e0321025326292b76cf8f6eb
Garjelin <sergeyyakimov89@gmail.com>
2025-12-27 12:21:18 +0500

    2025-12-27_2

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+struct DataPoint {
+    double x;
+    double y;
+};
 
 int main() {
-    std::cout << "Hello, World! Привет от C++ проекта!\n";
+    std::ifstream file("../train.dat");
+    
+    if (!file.is_open()) {
+        std::cerr << "Ошибка: не удалось открыть файл train.dat" << std::endl;
+        return 1;
+    }
+    
+    std::vector<DataPoint> data;
+    double x, y;
+    
+    while (file >> x >> y) {
+        data.push_back({x, y});
+    }
+    
+    file.close();
+    
+    std::cout << "Данные из файла train.dat (функция y = x^2):" << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "   x\t\t   y" << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+    
+    for (const auto& point : data) {
+        std::cout << "   " << point.x << "\t\t   " << point.y << std::endl;
+    }
+    
+    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "Всего точек: " << data.size() << std::endl;
+    
     return 0;
 }
